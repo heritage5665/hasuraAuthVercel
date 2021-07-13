2dbb6266f7a350c2826ba30873c9ac9c38fb6214
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-07-13 12:32:38 +0100

    uploads endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import pkg from "body-parser";
 import express from "express";
 import router from "./routes/user_route.js";
-// import { verifyToken } from "./utils/validate-token.js";
 import { AuthWebHook } from "./utils/web-hook-auth.js";
 import sgMail from "@sendgrid/mail";
-
-
+import { verifyToken } from "./utils/validate-token.js";
+import { UploadToCloudinary } from "./utils/uploads.js";
+import fileUpload from "express-fileupload";
 const { json } = pkg
-
+import { v2 as cloudinary } from "cloudinary";
 const app = express();
 app.use(json());
+app.use(fileUpload())
 app.post("/web-auth", AuthWebHook)
+app.post("/upload", verifyToken, UploadToCloudinary)
 app.use("/user", router);
 
 // PORT
 const PORT = process.env.PORT || 4000;
 sgMail.setApiKey("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");
+cloudinary.config({
+  cloud_name: 'techbird',
+  api_key: "456833922673438",
+  api_secret: "W-MU7sUefd_tk8PmUr3sFLSGYRw",
+  // secure: true
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server Started at PORT ${PORT}`);
---
+++ src/utils/uploads.ts
@@ -0,0 +1,43 @@
+import { Response, NextFunction } from "express";
+import { v2 as cloudinary } from "cloudinary";
+
+
+// middleware to validate token
+export const UploadToCloudinary = async (req: any, res: Response, next: NextFunction) => {
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    const file = req.files.media
+    const image_mimes = ["image/jpeg", "image/png", "image/gif", "image/avif"]
+    const audio_or_video_mime_types = ["audio/mp4", "audio/mp3", "video/3gp", "video/mp4", "video/quicktime"]
+    try {
+        if (image_mimes.includes(file.mimitype)) {
+            return await cloudinary.uploader.upload(file, {
+                tags: 'convoy_uploads',
+                resource_type: "image",
+                public_id: "convoy/auth/images/"
+            })
+        }
+        if (audio_or_video_mime_types.includes(file.mimetype)) {
+            return await cloudinary.uploader.upload_large(file, {
+                tags: 'convoy_uploads',
+                resource_type: "video",
+                public_id: "convoy/uploads/videos/"
+            })
+        }
+        return await cloudinary.uploader.upload(file, {
+            tags: 'convoy_uploads',
+            resource_type: "image",
+            public_id: "convoy/upload/others"
+        })
+
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+
+
+
+
+};
