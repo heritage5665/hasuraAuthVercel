ef307e211a14cc7cf1c96d596993de9cebcff5f6
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-07-13 17:46:17 +0100

    minor changes

--- src/hasura/client.ts
+++ src/hasura/client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosResponse } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 // import HasuraHeader from "./types/User"
 
 declare module 'axios' {
@@ -6,19 +6,12 @@ declare module 'axios' {
     }
 }
 
-export default abstract class HasuraHttpClient {
+abstract class HttpClient {
     protected readonly instance: AxiosInstance;
 
-    constructor() {
+    constructor(option: AxiosRequestConfig) {
 
-        this.instance = axios.create(
-            {
-                baseURL: 'https://convey-core.herokuapp.com/v1/graphql',
-                headers: {
-                    "X-Hasura-Admin-Secret": "uAi8w7bI0h40Dmgxl2PvOooaEI1DeNoPtdYn93TjgmJKraHjT3aseuQHOy3aZGCv"
-                }
-            }
-        )
+        this.instance = axios.create(option)
         this._initializeResponseInterceptor();
     }
 
@@ -38,9 +31,47 @@ export default abstract class HasuraHttpClient {
     }
 
     private _handleError = (error: any) => Promise.reject(error)
+
+}
+
+
+
+export default abstract class HasuraHttpClient extends HttpClient {
+
+    constructor() {
+        super({
+            baseURL: 'https://convey-core.herokuapp.com/v1/graphql',
+            headers: {
+                "X-Hasura-Admin-Secret": "uAi8w7bI0h40Dmgxl2PvOooaEI1DeNoPtdYn93TjgmJKraHjT3aseuQHOy3aZGCv"
+            }
+        })
+
+    }
     public runQuuery = (query: string, variables: object) => this.instance.post("", {
         "variables": variables,
         "query": query
     });
 }
 
+
+
+export class UploadHttpClient extends HttpClient {
+
+    constructor() {
+        super({
+            baseURL: 'https://agile-falls-17062.herokuapp.com/upload',
+            headers: {
+                'Content-Type': "multipart/form-data;",
+                "X-Hasura-Admin-Secret": "uAi8w7bI0h40Dmgxl2PvOooaEI1DeNoPtdYn93TjgmJKraHjT3aseuQHOy3aZGCv"
+            }
+        })
+
+    }
+    upload(file_type: string, file: any) {
+        return this.instance.post('/' + file_type, {
+            media: file
+        })
+
+    }
+
+}
--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -1,7 +1,7 @@
 import { Response, NextFunction } from "express";
-import { v2 as cloudinary } from "cloudinary";
 import DatauriParser from 'datauri/parser.js';
-import datauri from "datauri";
+
+import { UploadHttpClient } from "../hasura/client";
 
 import path from 'path';
 
@@ -9,7 +9,7 @@ const getFileFromBuffer = (req: any) => new DatauriParser()
     .format(path.extname(req.file.originalname).toString(), req.file.buffer)
 
 
-
+const uploadMan = new UploadHttpClient();
 
 
 // middleware to validate token
@@ -18,26 +18,16 @@ export const UploadToCloudinary = async (req: any, res: Response, next: NextFunc
     if (!req.file) {
         return res.status(400).json({ error: 'No files were uploaded.' });
     }
-    const uploaded_file = getFileFromBuffer(req);
-    const mimetype = uploaded_file.mimetype
-    return mimetype;
-    // const content = uploaded_file.content
-    // if (!content) {
-    //     return res.status(400).json({ error: 'No files were uploaded.' });
-    // }
-    // // console.log(file)
-    // try {
-    //     const res = await cloudinary.uploader.upload_large(content, {
-    //         tags: 'convoy_uploads',
-    //         resource_type: "auto",
-    //         // public_id: "convoy/upload/others"
-    //     })
-    //     return res.status(201).json({ data: res })
 
-    // } catch (err) {
-    //     console.log(err)
-    //     return res.status(200).json(err)
-    // }
+    const mimetype = req.file.mimetype
+    if (["audio/mp3", "audio/mp4", "video/3gp", "video/mp4"].includes(mimetype)) {
+        const res = uploadMan.upload("video", req.file)
+    } else {
+        const res = uploadMan.upload("image", req.file)
+    }
+    return res.status(201).json(res)
+
+
 
 
 
