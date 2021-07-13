a0f65b92de7708df652049e461eff419f7d0fbd1
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-07-13 16:47:43 +0100

    minor changes

--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -5,7 +5,7 @@ import DatauriParser from 'datauri/parser.js';
 import path from 'path';
 
 const getFileFromBuffer = (req: any): string => {
-    const file = new DatauriParser().format(path.extname(req.file.originalname).toString(), req.file.buffer)?.content
+    const file = new DatauriParser().format(path.extname(req.file.originalname).toString(), req.file.buffer)?.base64
     if (file == undefined) {
         throw new Error("no file given")
     }
@@ -24,30 +24,12 @@ export const UploadToCloudinary = async (req: any, res: Response, next: NextFunc
     const file_mimetype = req.file.mimetype
     const file = getFileFromBuffer(req);
     // console.log(file)
-
-
     try {
-        if (["image/jpeg", "image/png", "image/gif", "image/avif"].includes(file_mimetype)) {
-            const res = await cloudinary.uploader.upload(file, {
-                tags: 'convoy_uploads',
-                // resource_type: "image",
-                // public_id: "convoy/auth/images/"
-            })
-        }
-        if (["audio/mp4", "audio/mp3", "video/3gp", "video/mp4", "video/quicktime"].includes(file_mimetype)) {
-            const res = await cloudinary.uploader.upload_large(file, {
-                tags: 'convoy_uploads',
-                // resource_type: "video",
-                // public_id: "convoy/uploads/videos/"
-            })
-        }
-
-        const res = await cloudinary.uploader.upload(file, {
+        const res = await cloudinary.uploader.upload_large(file, {
             tags: 'convoy_uploads',
             resource_type: "auto",
             // public_id: "convoy/upload/others"
         })
-
         return res.status(201).json({ data: res })
 
     } catch (err) {
