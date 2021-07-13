4ea0610cef2711bc3320ead579657ecface10be8
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-07-13 17:05:33 +0100

    testing

--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -1,16 +1,14 @@
 import { Response, NextFunction } from "express";
 import { v2 as cloudinary } from "cloudinary";
 import DatauriParser from 'datauri/parser.js';
+import datauri from "datauri";
 
 import path from 'path';
 
-const getFileFromBuffer = (req: any): string => {
-    const file = new DatauriParser().format(path.extname(req.file.originalname).toString(), req.file.buffer)?.base64
-    if (file == undefined) {
-        throw new Error("no file given")
-    }
-    return file
-};
+const getFileFromBuffer = (req: any) => new DatauriParser()
+    .format(path.extname(req.file.originalname).toString(), req.file.buffer)
+
+
 
 
 
@@ -20,22 +18,26 @@ export const UploadToCloudinary = async (req: any, res: Response, next: NextFunc
     if (!req.file) {
         return res.status(400).json({ error: 'No files were uploaded.' });
     }
+    const uploaded_file = getFileFromBuffer(req);
+    const mimetype = uploaded_file.mimetype
+    return mimetype;
+    // const content = uploaded_file.content
+    // if (!content) {
+    //     return res.status(400).json({ error: 'No files were uploaded.' });
+    // }
+    // // console.log(file)
+    // try {
+    //     const res = await cloudinary.uploader.upload_large(content, {
+    //         tags: 'convoy_uploads',
+    //         resource_type: "auto",
+    //         // public_id: "convoy/upload/others"
+    //     })
+    //     return res.status(201).json({ data: res })
 
-    const file_mimetype = req.file.mimetype
-    const file = getFileFromBuffer(req);
-    // console.log(file)
-    try {
-        const res = await cloudinary.uploader.upload_large(file, {
-            tags: 'convoy_uploads',
-            resource_type: "auto",
-            // public_id: "convoy/upload/others"
-        })
-        return res.status(201).json({ data: res })
-
-    } catch (err) {
-        console.log(err)
-        return res.status(200).json(err)
-    }
+    // } catch (err) {
+    //     console.log(err)
+    //     return res.status(200).json(err)
+    // }
 
 
 
