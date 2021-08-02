de30c12f923ed2eb9d106f8eb93be7b72b9dcf8a
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-08-02 12:19:33 +0100

    less coupled upload endpoint

--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -2,72 +2,47 @@ import { Response, NextFunction } from "express";
 // import DatauriParser from 'datauri/parser.js';
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from 'streamifier'
-// middleware to validate token
-// export const UploadToCloudinary = async (req: any, res: Response, next: NextFunction) => {
 
+const uploadPipe = (result: any, error: any) => {
+    if (result) {
+        Promise.resolve(result);
+    } else {
+        Promise.resolve(error)
+    }
+}
+const uploadStream = (resource_type: any) => {
+    if (resource_type == undefined) {
+        return (req: any) => streamifier.createReadStream(req.file.buffer).pipe(cloudinary.uploader.upload_stream(uploadPipe))
+    }
+    return (req: any) => streamifier.createReadStream(req.file.buffer).pipe(cloudinary.uploader.upload_stream({ resource_type }, uploadPipe));
+}
+const upload_media = (media_type: any) => {
+    if (!['image', 'video', 'audio', 'application'].includes(media_type)) {
+        Promise.reject("unsupported media type")
+    }
 
-//     if (!req.file) {
-//         return res.status(400).json({ error: 'No files were uploaded.' });
-//     }
-//     let type = "image"
-//     if (["audio/mp3", "audio/mp4", "video/3gp", "video/mp4"].includes(req.file.mimetype)) {
-//         type = "video"
-//     }
-
-//     const uploadMan = new UploadHttpClient();
-//     return res.status(201).json(await uploadMan.upload(type, req.file))
-
-// };
-const upload_image = (req: any) => {
-    return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream(
-            (error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
-            }
-        );
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-};
-const upload_video = (req: any) => {
-    return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream({ "resource_type": "video" },
-            (error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
-            }
-        );
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-};
+    if (media_type == 'image' || media_type == undefined) {
+        return uploadStream("image")
+    }
+    if (media_type = "audio" || media_type == "video") {
+        return uploadStream("video")
+    }
+    return uploadStream("raw")
+}
 
 export const UploadToCloudinary = async function (req: any, res: Response, next: NextFunction) {
     if (req.file == undefined) {
         return res.status(400).json({ "msg": "media is required", 'error': 'Bad Request' })
     }
     const mimetype: string = req.file.mimetype
-    const is_image = mimetype.split("/")[0] == "image"
-    const is_video_or_audio = mimetype.split("/")[0] == "audio" || mimetype.split("/")[0] == "video"
-    // return res.status(200).json({ mimetype, is_image, is_video_or_audio })
-    let result
+    // const is_image = mimetype.split("/")[0] == "image"
+    // const is_video_or_audio = mimetype.split("/")[0] == "audio" || mimetype.split("/")[0] == "video"
+    // // return res.status(200).json({ mimetype, is_image, is_video_or_audio })
     try {
         // if (images_regex.exec(req.file.path)) {
 
-        if (is_image) {
-            result = await upload_image(req).catch(error => res.status(400).json(error))
-        }
-        if (is_video_or_audio) {
-            result = await upload_video(req).catch(error => res.status(400).json(error))
-        }
-
+        const media_type = mimetype.split("/")[0]
+        const result = upload_media(media_type)(req)
         return res.status(201).json(result);
 
     } catch (error) {
