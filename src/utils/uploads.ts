4c85aef2b52e8f7dc9a0a77922f05990cea48885
adegbite yusuf adegoke <4871592-techBBird@users.noreply.gitlab.com>
2021-08-02 16:48:07 +0100

    changes

--- src/utils/uploads.ts
+++ src/utils/uploads.ts
@@ -3,63 +3,51 @@ import { Response, NextFunction } from "express";
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from 'streamifier'
 
-const uploadPipe = (result: any, error: any) => {
-    if (result) {
-        Promise.resolve(result);
-    } else {
-        Promise.resolve(error)
-    }
-}
-const uploadStream = (resource_type: any) => {
-    if (resource_type == undefined) {
-        return (req: any) => streamifier.createReadStream(req.file.buffer).pipe(cloudinary.uploader.upload_stream(uploadPipe))
-    }
-    return (req: any) => streamifier.createReadStream(req.file.buffer).pipe(cloudinary.uploader.upload_stream({ resource_type }, uploadPipe));
-}
-const upload_media = (media_type: any) => {
-    if (!['image', 'video', 'audio', 'application'].includes(media_type)) {
-        Promise.reject("unsupported media type")
+const uploadStream = (req: any) => {
+
+    const mimetype: string = req.file.mimetype
+    let type: string = mimetype.split("/")[0]
+    let resource_type: string
+    switch (type) {
+        case 'image':
+            resource_type = 'image'
+        case 'audio':
+        case 'video':
+            resource_type = "video"
+            break;
+        default:
+            resource_type = "raw"
     }
 
-    if (media_type == 'image' || media_type == undefined) {
-        return uploadStream("image")
-    }
-    if (media_type = "audio" || media_type == "video") {
-        return uploadStream("video")
-    }
-    return uploadStream("raw")
-}
+    return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream({ resource_type },
+            (error, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            }
+        );
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+};
+
 
 export const UploadToCloudinary = async function (req: any, res: Response, next: NextFunction) {
     if (req.file == undefined) {
         return res.status(400).json({ "msg": "media is required", 'error': 'Bad Request' })
     }
-    const mimetype: string = req.file.mimetype
-    const is_image = mimetype.split("/")[0] == "image"
-    const is_video_or_audio = mimetype.split("/")[0] == "audio" || mimetype.split("/")[0] == "video"
-    let result
+
     try {
         // if (images_regex.exec(req.file.path)) {
-        return res.status(200).json({ mimetype, is_image, is_video_or_audio })
-        // let result
-        // try {
-        //     // if (images_regex.exec(req.file.path)) {
+        const result = await uploadStream(req).catch(error => res.status(400).json({ error }))
+        return res.status(200).json({ "data": result })
 
-        //     if (is_image) {
-        //         result = await upload_image(req).catch(error => res.status(400).json(error))
-        //     }
-        //     if (is_video_or_audio) {
-        //         result = await upload_video(req).catch(error => res.status(400).json(error))
-        //     } else {
-        //         console.error(mimetype)
-        //         return res.status(400).json({ "msg": "invalid image, audio or video given", 'error': 'Bad Request' })
-        //     }
-
-        //     return res.status(201).json(result);
-
-        // } catch (error) {
-        //     return res.status(400).json({ "error": "try again latter" })
-        // }
-
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ "msg": "error occured try again latter" })
     }
 
+}
