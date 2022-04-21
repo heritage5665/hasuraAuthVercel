24f212de6c44f12ef5c784321786558da22acb40
techBBird <adegokeddj236@gmail.com>
2022-04-21 09:49:38 +0100

    migrating to vercel

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,13 @@
 import pkg from "body-parser";
-import multer from 'multer';
 import express from "express";
+import router from "./routes/user_route.js";
+import price_router from "./routes/price_recommedation.js";
+import multer from 'multer';
+import { AuthWebHook } from "./utils/web-hook-auth.js";
 import sgMail from "@sendgrid/mail";
-import router from "../src/routes/user_route.js";
-import price_router from "../src/routes/price_recommedation.js";
-import { AuthWebHook } from "../src/utils/web-hook-auth.js";
-import { verifyToken } from "../src/utils/validate-token.js";
-import { UploadToCloudinary } from "../src/utils/uploads.js";
-import { SENDGRID_KEY } from "../src/config/settings.js";
+import { verifyToken } from "./utils/validate-token.js";
+import { UploadToCloudinary } from "./utils/uploads.js";
+import { SENDGRID_KEY } from "./config/settings.js";
 import { v2 as cloudinary } from "cloudinary";
 
 const app = express();
@@ -17,10 +17,10 @@ const PORT = process.env.PORT || 4000;
 sgMail.setApiKey(SENDGRID_KEY);
 
 cloudinary.config({
-    cloud_name: 'techbird',
-    api_key: "456833922673438",
-    api_secret: "W-MU7sUefd_tk8PmUr3sFLSGYRw",
-    // secure: true
+  cloud_name: 'techbird',
+  api_key: "456833922673438",
+  api_secret: "W-MU7sUefd_tk8PmUr3sFLSGYRw",
+  // secure: true
 });
 
 app.use(pkg.json());
@@ -29,6 +29,9 @@ app.use("/price_estimate", price_router);
 app.post("/upload", fileUpload.single('media'), verifyToken, UploadToCloudinary)
 app.use("/user", router);
 
-app.listen(PORT, () => {
-    console.log(`Server Started at PORT ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server Started at PORT ${PORT}`);
+// });
+
+
+module.exports = app;
