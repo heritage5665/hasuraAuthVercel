de7af3b1db6649a1a423803edf1366bd4ed6b5d4
techBBird <adegokeddj236@gmail.com>
2022-04-21 10:23:23 +0100

    finalize deployingment

--- api/index.ts
+++ api/index.ts
@@ -24,10 +24,25 @@ cloudinary.config({
 });
 
 app.use(pkg.json());
-app.post("/web-auth", AuthWebHook)
-app.use("/price_estimate", price_router);
-app.post("/upload", fileUpload.single('media'), verifyToken, UploadToCloudinary)
-app.use("/user", router);
+
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.post("/api/web-auth", AuthWebHook)
+app.use("/api/price_estimate", price_router);
+app.post("/api/upload", fileUpload.single('media'), verifyToken, UploadToCloudinary)
+app.use("/api/user", router);
 
 // app.listen(PORT, () => {
 //   console.log(`Server Started at PORT ${PORT}`);
