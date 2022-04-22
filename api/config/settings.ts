fbf472f7c62091d7c53237d49e5a6e4d6dd2b0fb
techBBird <adegokeddj236@gmail.com>
2022-04-22 09:24:16 +0100

    removed exposed secret keys

--- api/config/settings.ts
+++ api/config/settings.ts
@@ -1,8 +1,9 @@
 
-export const HASURA_GRAPHQL_ENGINE_BASE_URL = "https://convey-core.herokuapp.com/v1/graphql"
-export const HASURA_ADMIN_SECRET = "uAi8w7bI0h40Dmgxl2PvOooaEI1DeNoPtdYn93TjgmJKraHjT3aseuQHOy3aZGCv"
+
+export const HASURA_GRAPHQL_ENGINE_BASE_URL = process.env.HASURA_GRAPHQL_ENGINE_BASE_URL
+export const HASURA_ADMIN_SECRET = process.env.HASURA_ADMIN_SECRET
 export const ONE_TIME_PASSWORD_TOKEN_LENGTH = 7
-export const SENDGRID_KEY = "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"
+export const SENDGRID_KEY = process.env.SENDGRID_KEY
 export const TOKEN_EXPIRED_IN = 60 * 24
 export const MAIL_FROM = "support@convoy.com"
 export const USER_TABLE = "users"
