f2e0fcefb5451ea04018e9a6539b252da42a58c7
techBBird <adegokeddj236@gmail.com>
2022-04-28 11:26:05 +0100

    refreshToken

--- api/hasura/user_client.ts
+++ api/hasura/user_client.ts
@@ -64,7 +64,6 @@ export default class UserClient extends HasuraHttpClient {
                 expired_at
                 user {
                     user_id email phone user_type isVerified password 
-                    stores{ store_id store_name  } 
                     referral:invitation_code{code} 
                 }
             }
